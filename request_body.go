06bb9665adf7abeec7200a3045c3c5806c26082d
Anton Argirov <anton.argirov@gmail.com>
2026-03-24 11:55:35 +0700

    Add `WithRequestUpload` helper for multipart file uploads with optional metadata and description support to `WithRequestBodySchema`

--- request_body.go
+++ request_body.go
@@ -64,10 +64,11 @@ func WithRequestBody(rb *v320.RequestBody) RouteConfigFunc {
 	}
 }
 
-func WithRequestBodySchema(mime string, s *v320.Schema) RouteConfigFunc {
+func WithRequestBodySchema(mime string, description string, s *v320.Schema) RouteConfigFunc {
 	return func(rw *RouteWrapper) *RouteWrapper {
 		rw.RequestBodySchema[mime] = s
 		rw.Operation.AddRequestBody(&v320.RequestBody{
+			Description: description,
 			Content: map[string]*v320.MediaTypeObject{
 				mime: {
 					Schema: &v320.Ref[v320.Schema]{
@@ -91,5 +92,35 @@ func WithRequestBodyRef(name string) RouteConfigFunc {
 		}
 		rw.Operation.AddRequestBodyRef(fmt.Sprintf("#/components/requestBodies/%s", name))
 		return rw
+	}
+}
+
+func WithRequestUpload(metadataProperty string, description string, metadata any) RouteConfigFunc {
+	t := reflect.TypeOf(metadata)
+	if metadataProperty != "" && metadata != nil && t.Kind() != reflect.Struct {
+		panic(fmt.Errorf("echopen: struct expected, received %s", t.Kind()))
+	}
+
+	return func(rw *RouteWrapper) *RouteWrapper {
+		required := []string{"file"}
+		properties := map[string]*v320.Ref[v320.Schema]{
+			"file": {
+				Value: &v320.Schema{
+					Type:   "string",
+					Format: "binary",
+				},
+			},
+		}
+		if metadataProperty != "" && metadata != nil {
+			metadataRef := rw.API.ToSchemaRef(metadata)
+			required = append(required, metadataProperty)
+			properties[metadataProperty] = metadataRef
+		}
+		fn := WithRequestBodySchema("multipart/form-data", description, &v320.Schema{
+			Type:       "object",
+			Required:   required,
+			Properties: properties,
+		})
+		return fn(rw)
 	}
 }
